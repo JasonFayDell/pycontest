c0293b9913a849958f58be968276b8bea7bc263c
Jason_Fay <jason.fay@dell.com>
2019-05-02 10:39:28 -0500

    updated test cases

--- tests/test_elastic_collisions_1d.py
+++ tests/test_elastic_collisions_1d.py
@@ -9,5 +9,24 @@ from pycontest.utils import momentum, E_kin
 # using default values of m1 and m2
 def test_collision_1d_1():
     v1_f, v2_f = ec.collision_1d(v1_i=1, v2_i=-2)
+    assert v1_f == -2
+    assert v2_f == 1
 
+def test_collision_1d_2():
+    v1_f, v2_f = ec.collision_1d(v1_i=1, v2_i=-2, m1=2)
+    assert v1_f == -1
+    assert v2_f == 2
 
+def test_collision_1d_3():
+    v1_f, v2_f = ec.collision_1d(v1_i=1, v2_i=-2, m1=2, m2=2)
+    assert v1_f == -2
+    assert v2_f == 1
+
+def test_collision_1d_4():
+    v1_f, v2_f = ec.collision_1d(v1_i=1, v2_i=-2, m2=2)
+    assert v1_f == -3
+    assert v2_f == 0
+
+def test_collision_1d_5():
+    v1_f, v2_f = ec.collision_1d(v1_i=1, v2_i=-2, m1=1, m2=1e6)
+    assert v2_f == pytest.approx(-2, rel=1e-3)
